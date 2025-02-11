b66d2c0ce423c8e672ecf604cd7398e993bf53f9
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2025-02-11 15:52:00 +0530

    Collection
    Practice

--- COLLECTIONPRACTICE/src/TEST1.java
+++ COLLECTIONPRACTICE/src/TEST1.java
@@ -1,6 +1,7 @@
 // List for primitive Integer, Double, String
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -23,10 +24,48 @@ public class TEST1 {
 	       while(itr.hasNext()) { 
 	    	   System.out.println(itr.next());
 	       }
-	        
-	        
-	        
-
+	       //insert at begin , end and any position
+	       System.out.println("Before Insert");
+	       System.out.println(list);
+	       list.add(0,100);
+	       System.out.println("After Insertion");
+	       System.out.println(list);
+	       System.out.println(list.size());
+	       list.add(list.size(),200);
+	       System.out.println(list);
+	       list.add(3,300);
+	       System.out.println(list);
+	       List<Integer> sublist= Arrays.asList(10,20,30);
+	       System.out.println(list.addAll(sublist));
+	       System.out.println(list);
+	       //delete object from list
+	       list.remove(1);
+	       System.out.println(list);
+	      // list.remove(10);// 10 is index
+	       System.out.println(list);
+	       list.remove(new Integer(10)); //10 is object
+	       System.out.println(list);
+	       list.removeFirst();
+	       System.out.println(list);
+	       list.removeLast();
+	       System.out.println(list);
+	       boolean exist=list.contains(new Integer(10));
+	       if(exist)
+	    	   System.out.println("searched found");
+	       else
+	    	   System.out.println("Not found");
+	    
+	       //find index of an object
+	       System.out.println("index of 15 is "+
+	       list.indexOf(new Integer(15)));
+	       System.out.println("index of 15 is "+
+	    	       list.lastIndexOf(new Integer(15)));
+           list.add(200);
+           System.out.println(list);
+           System.out.println("index of 200 is "+
+        	       list.indexOf(new Integer(200)));
+        	       System.out.println("index of 200 is "+
+        	    	       list.lastIndexOf(new Integer(200)));
 	}
 
 }
